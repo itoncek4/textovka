b736f5c711f9260f7617afecbaf39f31d9bafa76
itoncek4 <itoncek4@gmail.com>
2020-01-21 17:05:25 +0100

    XDD

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <?php
+if ( $cesta == "" ) {
+				echo "Prosím pøihlašte se níže";
+				$cesta = "login";
+				} 
 require_once ( "login.php" );
 require_once ( "hesla.php" );
 $heslo = $_POST["r_heslo"];
 $jmeno = $_POST["r_uzivatel"];
-echo $jmeno;
-if ( $heslo == $hesla[$jmeno]["heslo"] )
-				 echo "true"; 
-                 
+if ( empty ( $heslo ) )
+				 die();
+if ( $heslo == $hesla[$jmeno]["heslo"] ) {
+				echo "true <br>";
+				 echo "Jméno: $jmeno <br>";
+				 echo "Celé jméno:" . $hesla[$jmeno]["jmeno"];
+				 $cesta = "system";
+} 
