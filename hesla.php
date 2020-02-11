eed4e0265ca19cca94ceab9c3034611f63a3669c
itoncek4 <itoncek4@gmail.com>
2020-02-11 15:26:52 +0100

    Update

--- hesla.php
+++ hesla.php
@@ -2,3 +2,12 @@
 $hesla["pepa"]["heslo"]="1234";
 $hesla["pepa"]["jmeno"]="frantisek vonasek"; 
 $hesla["pepa"]["email"]="ff@gg.dd";
+$hesla["josef"]["heslo"]="4321";
+$hesla["josef"]["jmeno"]="Josef Josef"; 
+$hesla["josef"]["email"]="bagr@bagr.bagr";
+$hesla["arnost"]["heslo"]="1111";
+$hesla["arnost"]["jmeno"]="arnost"; 
+$hesla["arnost"]["email"]="arnost@bagr.cz";
+$hesla["adolf"]["heslo"]="hitler";
+$hesla["adolf"]["jmeno"]="Adolf Hitler"; 
+$hesla["adolf"]["email"]="adolf@bagr.cz";
